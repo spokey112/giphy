73d0caaca563cd1898d9907925cd26ba59bd57a4
Joseph Ramos <spokey112@gmail.com>
2019-04-03 21:58:44 +0000

    oof

--- js/script.js
+++ js/script.js
@@ -4,8 +4,15 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
-  
-});
-
+    
+    var search= $("input").val();
+    
+  $.ajax({
+      url:"https://api.giphy.com/v1/gifs/search?q=" + search + "&rating=pg&api_key=dc6zaTOxFJmzC",
+      method:"GET",
+      success: function(response) 
+      {
+        $(".container").html("<img src=" + response.data[0].images.original.url + ">");
+      }
+  });
+  });
